b655a7e674f061a7b921e02bce298c35d6bf29ff
Argenis Osorio <argenisosorio580@hotmail.com>
2022-03-05 13:15:35 -0400

    Creado AuthController y agregada ruta register en api routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create($request->all());
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 200);
+    }
+}
