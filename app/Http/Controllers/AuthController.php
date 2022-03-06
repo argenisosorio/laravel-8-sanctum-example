37f2c37d9b628f6a429d4e15703a949ef8ca996b
Argenis Osorio <argenisosorio580@hotmail.com>
2022-03-05 22:40:52 -0400

    Agregado método login en AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -35,5 +36,35 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'Bearer'
         ], 200);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login, method that receives data from a request, compares it with the data
+    | stored in the database and returns a json response.
+    |--------------------------------------------------------------------------
+    */
+    public function login(Request $request)
+    {
+        $attr = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!Auth::attempt($attr)) {
+            return response()->json([
+                'message' => 'The data is invalid',
+            ], 401);
+        }
+        else {
+            $user = User::where('email', $request['email'])->firstOrFail();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ], 200);
+        }
     }
 }
